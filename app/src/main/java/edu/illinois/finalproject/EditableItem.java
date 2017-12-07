aeb53919cde1da633d70a3b57d177e8d75a1e463
Dillon Hammond <dillonhammond@gmail.com>
2017-12-07 02:50:46 -0600

    Get item info and populate editable

--- app/src/main/java/edu/illinois/finalproject/EditableItem.java
+++ app/src/main/java/edu/illinois/finalproject/EditableItem.java
@@ -1,7 +1,13 @@
 package edu.illinois.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by dillon on 12/7/17.
@@ -10,13 +16,55 @@ import android.support.v7.app.AppCompatActivity;
 public class EditableItem extends AppCompatActivity {
     private static String EDITABLE_ITEM_KEY = "editable_item";
 
+    private Item currentItem;
+
+    private ImageButton editImageButton;
+
+    private EditText editItemName;
+    private EditText editItemDescription;
+    private EditText editItemPrice;
+    private EditText editItemContactInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editable_item_with_image);
+
+        this.currentItem = extractItem();
+
+        this.editImageButton = findViewById(R.id.editImageButton);
+        Picasso.with(this).load(currentItem.getImageUrl()).into(this.editImageButton);
+        setUpButtons();
+
+        this.editItemName = findViewById(R.id.editItemName);
+        this.editItemDescription = findViewById(R.id.editItemDescription);
+        this.editItemPrice = findViewById(R.id.editItemPrice);
+        this.editItemContactInfo = findViewById(R.id.editItemContactInfo);
+        setUpElements();
+    }
+
+    private void setUpButtons() {
+        this.editImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Allow changing image
+            }
+        });
+    }
+
+    private void setUpElements() {
+        this.editItemName.setText(this.currentItem.getName());
+        this.editItemDescription.setText(this.currentItem.getDescription());
+        this.editItemPrice.setText("$" + this.currentItem.getPrice().toString());
+        this.editItemContactInfo.setText(this.currentItem.getContactInfo());
     }
 
     public static String getEditableItemKey() {
         return EDITABLE_ITEM_KEY;
+    }
+
+    private Item extractItem() {
+        final Intent intent = getIntent();
+        return intent.getParcelableExtra(EDITABLE_ITEM_KEY);
     }
 }
