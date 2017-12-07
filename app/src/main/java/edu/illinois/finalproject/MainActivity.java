608e98fbec57eebb7e1b6217ca6c5231e8c6462a
Dillon Hammond <dillonhammond@gmail.com>
2017-12-06 23:25:16 -0600

    Start filling out main

--- app/src/main/java/edu/illinois/finalproject/MainActivity.java
+++ app/src/main/java/edu/illinois/finalproject/MainActivity.java
@@ -1,14 +1,39 @@
 package edu.illinois.finalproject;
 
+import android.support.design.widget.TabItem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabItem allItemsTab;
+    private TabItem myItemsTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        allItemsTab = findViewById(R.id.allItemsTab);
+        myItemsTab = findViewById(R.id.myItemsTab);
+        setUpTabs();
+
+
+    }
+
+    private void setUpTabs() {
+        allItemsTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Make sure the All Items loads
+            }
+        });
+        myItemsTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Make sure My Items loads
+            }
+        });
     }
 }
