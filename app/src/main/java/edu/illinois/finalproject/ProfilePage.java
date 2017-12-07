5f6b4bb8318ebc95177f001aaa5d6f91cfa1312d
Dillon Hammond <dillonhammond@gmail.com>
2017-12-07 01:47:43 -0600

    Can access my profile

---
+++ app/src/main/java/edu/illinois/finalproject/ProfilePage.java
@@ -0,0 +1,17 @@
+package edu.illinois.finalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dillon on 12/7/17.
+ */
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+    }
+}
