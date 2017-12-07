373ee78d6e4e945899bfa20aee6d70db208684ca
Dillon Hammond <dillonhammond@gmail.com>
2017-12-06 23:52:18 -0600

    Barebones detailed view

---
+++ app/src/main/java/edu/illinois/finalproject/DetailedItem.java
@@ -0,0 +1,13 @@
+package edu.illinois.finalproject;
+
+/**
+ * Created by dillon on 12/6/17.
+ */
+
+public class DetailedItem {
+    private static String ITEM_KEY = "item";
+
+    public static String getItemKey() {
+        return ITEM_KEY;
+    }
+}
