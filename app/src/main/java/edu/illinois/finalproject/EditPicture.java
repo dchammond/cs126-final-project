db07e7a34b879aaaa9bce2599180ae6d483c3bc9
Dillon Hammond <dillonhammond@gmail.com>
2017-12-07 02:56:57 -0600

    Start making edit imag epage

---
+++ app/src/main/java/edu/illinois/finalproject/EditPicture.java
@@ -0,0 +1,10 @@
+package edu.illinois.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dillon on 12/7/17.
+ */
+
+public class EditPicture extends AppCompatActivity {
+}
